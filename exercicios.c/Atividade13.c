a059f1a17228e88f8fac274de53672082a64ca02
Waguinhoo <156625511+Wagnersilva05@users.noreply.github.com>
2024-03-25 09:09:49 -0300

    Add files via upload

---
+++ exercicios.c/Atividade13.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+int main(){
+	setlocale(LC_ALL, "");
+	
+	int i;
+	
+	
+	for (i = 100; i <= 120; i++){
+		if(i % 2 == 0){
+			printf("%d\n", i);
+		}
+	}
+	
+	
+	return 0;
+	
+}
