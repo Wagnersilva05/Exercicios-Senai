1d72cfe4fada1138cc1798b865f8225ddc3da364
Waguinhoo <156625511+Wagnersilva05@users.noreply.github.com>
2024-04-09 10:37:17 -0300

    Update atividade30.c

--- exercicios.c/atividade30.c
+++ exercicios.c/atividade30.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
-
+#include <locale.h>
 
 int main (){
 	
 	
-	 int nota[3];
-	 int i;
-	 int soma = 0;
-	 float media;
-	 
-	 
-	 printf("Digite as notas: \n");
-	 for (i = 0; i < 3; i++){
-	 	
-	 	printf("Nota %d: ", i + 1);
-	 	scanf("%d", &nota[i]);
-	 	
-	 	soma += (float)nota[i];
-	 }
-	 
-	 	media = (float)soma / 3;
-	 	
-	 printf("\nExibindo notas:\n");
-	 
-	 for(i = 0; i < 3; i++){
-	printf("\nNota %d: %d", i+1, nota[i]);
-	
+    int nota[3];
+    int i;
+    int soma = 0;
+    float media;
 
-}
-	if(media >= 7 ){
-		printf("Aprovado");
-	}
-	if(media >= 5){
-		printf("Recuperação");
-	}else{
-		printf("Reprovado");
-	}
-	
+    printf("Digite as notas: \n");
+    for (i = 0; i < 3; i++){
+        printf("Nota %d: ", i + 1);
+        scanf("%d", &nota[i]);
+
+        soma += (float)nota[i];
+    }
+
+    media = (float)soma / 3;
+
+    printf("\nExibindo notas:\n");
+    for(i = 0; i < 3; i++){
+        printf("\nNota %d: %d", i+1, nota[i]);
+    }
+
+    
     printf("\nMedia: %.1f", media); 
-	 return 0;
-	
+
+    if(media >= 7 ){
+        printf("\nAprovado");
+    } else if(media >= 5){
+        printf("\nRecuperação");
+    } else {
+        printf("\nReprovado");
+    }
+    return 0;
 }
